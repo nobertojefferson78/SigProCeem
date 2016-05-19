ffb6beb0e784087e5e16ed729bc3ea55e3e48608
Norberto <Norberto@thrusty>
2016-05-19 14:45:01 -0300

    tela cadastro aluno finalizada

--- src/telas/CadastroAluno.java
+++ src/telas/CadastroAluno.java
@@ -22,6 +22,13 @@ import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import java.awt.Font;
 import javax.swing.JSeparator;
+import java.awt.Panel;
+import java.awt.Label;
+import java.awt.Button;
+import java.awt.Canvas;
+import javax.swing.JEditorPane;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
 
 public class CadastroAluno extends JFrame {
 
@@ -29,7 +36,6 @@ public class CadastroAluno extends JFrame {
 	private JTextField txtNome;
 	private JTextField txtCpf;
 	private JTextField txtRg;
-	private JTextField txtOrgaoEmaissor;
 	private JTextField txtDataNascimento;
 	private JTextField txtTelefone;
 	private JTextField txtEmail;
@@ -74,7 +80,7 @@ public class CadastroAluno extends JFrame {
 	 */
 	public CadastroAluno() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 640, 480);
+		setBounds(100, 100, 831, 494);
 		
 		JMenuBar menuBar = new JMenuBar();
 		menuBar.setToolTipText("");
@@ -225,211 +231,236 @@ public class CadastroAluno extends JFrame {
 		JPanel panel = new JPanel();
 		panel.setBorder(new CompoundBorder());
 		panel.setBackground(SystemColor.activeCaption);
-		panel.setBounds(10, 25, 604, 215);
+		panel.setBounds(204, 6, 604, 264);
 		contentPane.add(panel);
 		panel.setLayout(null);
 		
 		JLabel lblCep = new JLabel("CEP: ");
-		lblCep.setBounds(213, 141, 40, 14);
+		lblCep.setBounds(209, 172, 40, 14);
 		panel.add(lblCep);
 		
-		JLabel lblCidade = new JLabel("Cidade: ");
-		lblCidade.setBounds(10, 141, 53, 14);
-		panel.add(lblCidade);
-		
-		JLabel lblBairro = new JLabel("Bairro: ");
-		lblBairro.setBounds(10, 115, 40, 14);
-		panel.add(lblBairro);
-		
 		JLabel lblNumero = new JLabel("Numero: ");
-		lblNumero.setBounds(447, 115, 53, 14);
+		lblNumero.setBounds(443, 139, 53, 14);
 		panel.add(lblNumero);
 		
 		JLabel lblLogradouro = new JLabel("Rua: ");
-		lblLogradouro.setBounds(10, 88, 40, 14);
+		lblLogradouro.setBounds(6, 106, 40, 14);
 		panel.add(lblLogradouro);
 		
 		JLabel lblEmail = new JLabel("Email: ");
-		lblEmail.setBounds(213, 59, 40, 14);
+		lblEmail.setBounds(209, 76, 40, 14);
 		panel.add(lblEmail);
 		
 		JLabel lblTelefone = new JLabel("Telefone:");
-		lblTelefone.setBounds(10, 63, 62, 14);
+		lblTelefone.setBounds(6, 76, 62, 14);
 		panel.add(lblTelefone);
 		
 		JLabel lblDataNasc = new JLabel("Data Nasc.");
-		lblDataNasc.setBounds(447, 38, 62, 14);
+		lblDataNasc.setBounds(401, 46, 62, 14);
 		panel.add(lblDataNasc);
 		
-		JLabel lblOrgoEmissor = new JLabel("Org\u00E3o Emissor");
-		lblOrgoEmissor.setBounds(315, 38, 76, 14);
-		panel.add(lblOrgoEmissor);
-		
 		JLabel lblRg = new JLabel("R.G.: ");
-		lblRg.setBounds(166, 38, 38, 14);
+		lblRg.setBounds(209, 46, 38, 14);
 		panel.add(lblRg);
 		
 		JLabel lblCpf = new JLabel("cpf: ");
-		lblCpf.setBounds(10, 38, 40, 14);
+		lblCpf.setBounds(6, 46, 40, 14);
 		panel.add(lblCpf);
 		
 		JLabel lblNome = new JLabel("Nome: ");
-		lblNome.setBounds(10, 13, 40, 14);
+		lblNome.setBounds(6, 13, 40, 14);
 		panel.add(lblNome);
 		
 		txtNome = new JTextField();
-		txtNome.setBounds(78, 6, 516, 28);
+		txtNome.setBounds(74, 6, 516, 28);
 		panel.add(txtNome);
 		txtNome.setColumns(10);
 		
 		txtCpf = new JTextField();
-		txtCpf.setBounds(78, 31, 76, 28);
+		txtCpf.setBounds(74, 39, 125, 28);
 		panel.add(txtCpf);
 		txtCpf.setColumns(10);
 		
 		txtRg = new JTextField();
-		txtRg.setBounds(203, 31, 100, 28);
+		txtRg.setBounds(250, 39, 125, 28);
 		panel.add(txtRg);
 		txtRg.setColumns(10);
 		
-		txtOrgaoEmaissor = new JTextField();
-		txtOrgaoEmaissor.setBounds(392, 31, 44, 28);
-		panel.add(txtOrgaoEmaissor);
-		txtOrgaoEmaissor.setColumns(10);
-		
 		txtDataNascimento = new JTextField();
-		txtDataNascimento.setBounds(509, 31, 85, 28);
+		txtDataNascimento.setBounds(475, 39, 115, 28);
 		panel.add(txtDataNascimento);
 		txtDataNascimento.setColumns(10);
 		
 		txtTelefone = new JTextField();
-		txtTelefone.setBounds(78, 56, 126, 28);
+		txtTelefone.setBounds(74, 69, 126, 28);
 		panel.add(txtTelefone);
 		txtTelefone.setColumns(10);
 		
 		txtEmail = new JTextField();
-		txtEmail.setBounds(254, 56, 340, 28);
+		txtEmail.setBounds(250, 69, 340, 28);
 		panel.add(txtEmail);
 		txtEmail.setColumns(10);
 		
 		txtRua = new JTextField();
-		txtRua.setBounds(78, 81, 516, 28);
+		txtRua.setBounds(74, 99, 516, 28);
 		panel.add(txtRua);
 		txtRua.setColumns(10);
 		
 		txtNumero = new JTextField();
-		txtNumero.setBounds(509, 106, 85, 28);
+		txtNumero.setBounds(505, 132, 85, 28);
 		panel.add(txtNumero);
 		txtNumero.setColumns(10);
 		
 		txtBairro = new JTextField();
-		txtBairro.setBounds(78, 106, 358, 28);
+		txtBairro.setBounds(74, 132, 358, 28);
 		panel.add(txtBairro);
 		txtBairro.setColumns(10);
 		
 		txtCidade = new JTextField();
-		txtCidade.setBounds(78, 134, 125, 28);
+		txtCidade.setBounds(74, 165, 125, 28);
 		panel.add(txtCidade);
 		txtCidade.setColumns(10);
 		
 		txtCep1 = new JTextField();
-		txtCep1.setBounds(254, 134, 100, 28);
+		txtCep1.setBounds(250, 165, 100, 28);
 		panel.add(txtCep1);
 		txtCep1.setColumns(10);
 		
 		txtCep2 = new JTextField();
-		txtCep2.setBounds(372, 134, 64, 28);
+		txtCep2.setBounds(368, 165, 64, 28);
 		panel.add(txtCep2);
 		txtCep2.setColumns(10);
 		
 		JLabel lblEstado = new JLabel("Estado: ");
-		lblEstado.setBounds(447, 141, 46, 14);
+		lblEstado.setBounds(450, 172, 46, 14);
 		panel.add(lblEstado);
 		
 		JComboBox cboxEstado = new JComboBox();
-		cboxEstado.setBounds(509, 134, 85, 28);
+		cboxEstado.setBounds(505, 165, 85, 28);
 		panel.add(cboxEstado);
 		
 		JLabel label = new JLabel("-");
-		label.setBounds(352, 141, 22, 14);
+		label.setBounds(348, 172, 22, 14);
 		panel.add(label);
 		label.setHorizontalAlignment(SwingConstants.CENTER);
 		
-		JLabel lblNomeMe = new JLabel("Nome M\u00E3e: ");
-		lblNomeMe.setBounds(10, 166, 69, 14);
-		panel.add(lblNomeMe);
-		
 		JLabel lblNomePai = new JLabel("Nome Pai: ");
-		lblNomePai.setBounds(10, 191, 69, 14);
+		lblNomePai.setBounds(6, 239, 69, 14);
 		panel.add(lblNomePai);
 		
 		txtNomeMae = new JTextField();
-		txtNomeMae.setBounds(78, 159, 516, 28);
+		txtNomeMae.setBounds(74, 199, 516, 28);
 		panel.add(txtNomeMae);
 		txtNomeMae.setColumns(10);
 		
 		txtNomePai = new JTextField();
-		txtNomePai.setBounds(78, 184, 516, 28);
+		txtNomePai.setBounds(74, 232, 516, 28);
 		panel.add(txtNomePai);
 		txtNomePai.setColumns(10);
 		
+		JLabel lblBairro = new JLabel("Bairro:");
+		lblBairro.setBounds(6, 138, 55, 16);
+		panel.add(lblBairro);
+		
+		JLabel lblCidade = new JLabel("Cidade:");
+		lblCidade.setBounds(6, 171, 55, 16);
+		panel.add(lblCidade);
+		
+		JLabel lblNomeMe = new JLabel("Nome M\u00E3e:");
+		lblNomeMe.setBounds(6, 205, 69, 16);
+		panel.add(lblNomeMe);
+		
 		JPanel panel_1 = new JPanel();
 		panel_1.setBackground(SystemColor.activeCaption);
-		panel_1.setBounds(10, 243, 604, 127);
+		panel_1.setBounds(10, 277, 798, 95);
 		contentPane.add(panel_1);
 		panel_1.setLayout(null);
 		
-		JLabel lblTurno = new JLabel("Turno: ");
-		lblTurno.setBounds(10, 11, 46, 14);
+		JLabel lblTurno = new JLabel("Turno de Aulas: ");
+		lblTurno.setBounds(532, 11, 91, 14);
 		panel_1.add(lblTurno);
 		
 		JComboBox cboxTurno = new JComboBox();
-		cboxTurno.setBounds(78, 4, 100, 28);
+		cboxTurno.setBounds(635, 4, 125, 28);
 		panel_1.add(cboxTurno);
 		
 		JLabel lblOpoLingua = new JLabel("Op\u00E7\u00E3o Lingua: ");
-		lblOpoLingua.setBounds(204, 15, 90, 14);
+		lblOpoLingua.setBounds(279, 11, 90, 14);
 		panel_1.add(lblOpoLingua);
 		
 		JComboBox cboxOpcaoLingua = new JComboBox();
-		cboxOpcaoLingua.setBounds(289, 4, 100, 28);
+		cboxOpcaoLingua.setBounds(376, 4, 125, 28);
 		panel_1.add(cboxOpcaoLingua);
 		
 		JLabel lblSituaoEscolar = new JLabel("Situa\u00E7\u00E3o Escolar:");
-		lblSituaoEscolar.setBounds(10, 52, 100, 14);
+		lblSituaoEscolar.setBounds(6, 11, 100, 14);
 		panel_1.add(lblSituaoEscolar);
 		
 		JRadioButton rbtnConcluindo2Ano = new JRadioButton("Concluindo 2\u00BA ano");
 		rbtnConcluindo2Ano.setBackground(SystemColor.activeCaption);
-		rbtnConcluindo2Ano.setBounds(113, 48, 136, 23);
+		rbtnConcluindo2Ano.setBounds(105, 7, 136, 23);
 		panel_1.add(rbtnConcluindo2Ano);
 		
 		JRadioButton rbtnConcluindo3Ano = new JRadioButton("Concluindo 3\u00BA ano");
 		rbtnConcluindo3Ano.setBackground(SystemColor.activeCaption);
-		rbtnConcluindo3Ano.setBounds(113, 73, 136, 23);
+		rbtnConcluindo3Ano.setBounds(105, 33, 136, 23);
 		panel_1.add(rbtnConcluindo3Ano);
 		
 		JRadioButton rbtnEnsinoMedioCompleto = new JRadioButton("Ensino m\u00E9dio completo");
 		rbtnEnsinoMedioCompleto.setBackground(SystemColor.activeCaption);
-		rbtnEnsinoMedioCompleto.setBounds(113, 98, 165, 23);
+		rbtnEnsinoMedioCompleto.setBounds(105, 59, 165, 23);
 		panel_1.add(rbtnEnsinoMedioCompleto);
 		
 		JLabel lblNotaPortugs = new JLabel("Nota Portugu\u00EAs: ");
-		lblNotaPortugs.setBounds(412, 18, 100, 14);
+		lblNotaPortugs.setBounds(279, 63, 100, 14);
 		panel_1.add(lblNotaPortugs);
 		
 		txtNotaPortugues = new JTextField();
-		txtNotaPortugues.setBounds(512, 8, 82, 28);
+		txtNotaPortugues.setBounds(376, 56, 125, 28);
 		panel_1.add(txtNotaPortugues);
 		txtNotaPortugues.setColumns(10);
 		
 		JLabel lblNotaMatemtica = new JLabel("Nota Matem\u00E1tica: ");
-		lblNotaMatemtica.setBounds(412, 40, 100, 14);
+		lblNotaMatemtica.setBounds(532, 63, 100, 14);
 		panel_1.add(lblNotaMatemtica);
 		
 		txtNotaMatematica = new JTextField();
-		txtNotaMatematica.setBounds(512, 33, 82, 28);
+		txtNotaMatematica.setBounds(635, 56, 125, 28);
 		panel_1.add(txtNotaMatematica);
 		txtNotaMatematica.setColumns(10);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBackground(SystemColor.activeCaption);
+		panel_2.setBounds(10, 6, 182, 264);
+		contentPane.add(panel_2);
+		panel_2.setLayout(null);
+		
+		JButton button = new JButton("");
+		button.setIcon(new ImageIcon("C:\\Users\\Norberto\\workspace\\SigProCeem\\image\\fotoInicial2.png"));
+		button.setEnabled(false);
+		button.setBounds(6, 6, 170, 172);
+		panel_2.add(button);
+		
+		JButton btnNovaImagem = new JButton("Nova Imagem");
+		btnNovaImagem.setBounds(17, 190, 146, 28);
+		panel_2.add(btnNovaImagem);
+		
+		JButton btnRemoverImagem = new JButton("Remover Imagem");
+		btnRemoverImagem.setBounds(17, 218, 146, 28);
+		panel_2.add(btnRemoverImagem);
+		
+		JPanel panel_3 = new JPanel();
+		panel_3.setBackground(SystemColor.activeCaption);
+		panel_3.setBounds(10, 377, 799, 47);
+		contentPane.add(panel_3);
+		panel_3.setLayout(null);
+		
+		JButton btnNewButton = new JButton("Salvar");
+		btnNewButton.setBounds(242, 6, 136, 35);
+		panel_3.add(btnNewButton);
+		
+		JButton btnCancelar = new JButton("Cancelar");
+		btnCancelar.setBounds(390, 6, 136, 35);
+		panel_3.add(btnCancelar);
 	}
 }
